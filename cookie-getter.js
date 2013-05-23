0cd0be23090556702c1e7fc506d1f2a9fb7dc758
Henrik Joreteg <hjoreteg@gmail.com>
2013-05-23 13:37:50 -0700

    adding JSON parsing

--- cookie-getter.js
+++ cookie-getter.js
@@ -2,6 +2,9 @@
 module.exports = function (name) {
     var cookie = document.cookie,
         setPos = cookie.indexOf(name + '='),
-        stopPos = cookie.indexOf(';', setPos);
-    return !~setPos ? null : cookie.substring(setPos, ~stopPos ? stopPos : undefined).split('=')[1];
+        stopPos = cookie.indexOf(';', setPos),
+        res;
+    if (!~setPos) return null;
+    res = decodeURIComponent(cookie.substring(setPos, ~stopPos ? stopPos : undefined).split('=')[1]);
+    return (res.charAt(0) === '{') ? JSON.parse(res) : res;
 };
